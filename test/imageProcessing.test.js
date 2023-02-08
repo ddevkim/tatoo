39a5cc3f82643005bc7acdd33a0ff89fd557f3f1
ddevkim <ddevkim@gmail.com>
2023-02-08 20:22:48 +0900

    chore: 이미지 프로세싱 테스트 코드

--- test/imageProcessing.test.js
+++ test/imageProcessing.test.js
@@ -2,9 +2,16 @@ import * as path from "path";
 
 import { getImageData, getImageToCanvas } from "../src/utils/canvasHandler";
 import { loadImg } from "../src/utils/imageHandler";
-import { convolute, imageDataTo2dArray } from "../src/utils/pixelManipulation";
+import {
+  convolute,
+  convoluteAlpha,
+  getGaussianKernel,
+  imageDataTo2dArray,
+  kernelValidation,
+} from "../src/utils/pixelManipulation";
+import { COLOR_GLUE } from "../src/constants";
 
-const kernel = [
+const TEST_KERNEL = [
   [1, 1, 1],
   [1, 1, 1],
   [1, 1, 1],
@@ -22,8 +29,8 @@ beforeEach(async () => {
 describe("캔버스 기본 테스트", () => {
   test("[getImageToCanvas] 이미지 => 캔버스 생성", () => {
     expect(canvas).not.toBeNull();
-    expect(canvas.width).toEqual(5);
-    expect(canvas.height).toEqual(5);
+    expect(canvas.width).toEqual(4);
+    expect(canvas.height).toEqual(4);
   });
   test("[getImageData] 캔버스 => 이미지 데이터", () => {
     const imageData = getImageData({ canvas });
@@ -31,11 +38,13 @@ describe("캔버스 기본 테스트", () => {
   });
 });
 
-describe("픽셀 조작 테스트", () => {
-  test("[convolute] 커널 컨볼루션 적용", () => {
+describe("다채널 픽셀 컨볼루션 테스트", () => {
+  test("[convolute] 4채널", () => {
     expect(
       imageDataTo2dArray({
-        imageData: getImageData({ canvas: convolute({ canvas, kernel }) }),
+        imageData: getImageData({
+          canvas: convolute({ canvas, kernel: TEST_KERNEL }),
+        }),
       })
     ).toEqual([
       [
@@ -63,5 +72,32 @@ describe("픽셀 조작 테스트", () => {
         [117, 146, 165, 255],
       ],
     ]);
+  });
+});
+
+describe("알파 채널 컨볼루션 + 픽셀 바이너리화", () => {
+  test("[kernelValidation] 커널 n x n 2D 스퀘어 행렬 검증", () => {
+    expect(() => kernelValidation({ kernel: 1 })).toThrow();
+    expect(() => kernelValidation({ kernel: [1, 2, 3] })).toThrow();
+    expect(() => kernelValidation({ kernel: [[[1, 2, 3], 2, 3]] })).toThrow();
+    expect(() => kernelValidation({ kernel: TEST_KERNEL })).not.toThrow();
+  });
+  test("[getGaussianKernel] 가우시안 커널 생성", () => {
+    expect(getGaussianKernel({ size: 3, sigma: 1.5 })).toEqual([
+      [0.04535423476987057, 0.05664058479678963, 0.04535423476987057],
+      [0.05664058479678963, 0.0707355302630646, 0.05664058479678963],
+      [0.04535423476987057, 0.05664058479678963, 0.04535423476987057],
+    ]);
+  });
+  test("[Pick alpha channel]", () => {
+    expect(
+      imageDataTo2dArray({
+        imageData: getImageData({
+          canvas: convoluteAlpha({ canvas, kernel: TEST_KERNEL }),
+        }),
+      })
+    ).toEqual(
+      [...Array(4)].map(() => [...Array(4)].map(() => [...COLOR_GLUE, 255]))
+    );
   });
 });
